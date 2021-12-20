4adf8e7ba882831e13f0e38749b62ee9cad0db6f
faysalshuvo <faysalshuvo.dev@gmail.com>
2021-12-20 18:35:14 +0600

    card design  DONE

--- client/src/components/Slider.jsx
+++ client/src/components/Slider.jsx
@@ -95,7 +95,7 @@ const Slider = () => {
       </Arrow>
       <Wrapper slideIndex={slideIndex}>
         {sliderItems.map((item) => (
-          <Slide bg={item.bg}>
+          <Slide bg={item.bg} item={item.id}>
             <ImgContainer>
               <Image src={item.img} />
             </ImgContainer>
