3e2aed90153706dc1b978a05ad3284a32c0d972b
faysalshuvo <faysalshuvo.dev@gmail.com>
2021-12-22 22:36:39 +0600

    mongoose models created

--- server/index.js
+++ server/index.js
@@ -2,16 +2,21 @@ const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
 require("dotenv").config();
+const userRouter = require("./routes/user");
+
+// middleware
+app.use(express.json());
 
 // connecting Mongoose
 const mongoURI = `mongodb+srv://${process.env.USER}:${process.env.PASSWORD}@cluster0.rvp8s.mongodb.net/shop?retryWrites=true&w=majority`;
-
 try {
-  // Connect to the MongoDB cluster
   mongoose.connect(mongoURI, () => console.log(" Mongoose is connected"));
 } catch (err) {
   console.log("could not connect", err);
 }
+
+// routes
+app.use("/api/user/", userRouter);
 
 app.listen(process.env.PORT || 4000, () => {
   console.log("post is 4000");
